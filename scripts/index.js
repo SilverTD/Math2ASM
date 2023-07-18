e9cb4b9ca57880fd988807a7e441dbfe21a23d4d
Thanh Duy <sologame37@gmail.com>
2023-07-18 22:20:15 +0700

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,76 +1,74 @@
-const expression = "(2 - 1) + (9 - 3) * 6 + 9 + 10 * (8 - 10)";
+const expression = "(6 - 3) + (9 - 3) * 6";
 const tokens = new Lexer(expression).lexer();
 const ast = new Parser(tokens).parse();
 const result = new Visitor(ast).visit();
 console.log(result);
 
-let stack_size = 0;
+let stack_size = 1;
 let store_address = [];
 
 const asm_instruction = {
-	"ADD": "addl",
-	"SUB": "subl",
-	"MULT": "imull",
-	"DIV": "div"
+        "ADD": "addl",
+        "SUB": "subl",
+        "MULT": "imull",
+        "DIV": "div"
 };
 
 function trim(str) {
-	return str.replaceAll('\t', "").trim();
+        return str.replaceAll('\t', "").trim();
 }
 
-function test(ast) {	
-	let root = ast;
+function test(ast) {
+        let root = ast;
 
-	if (root.left.value && root.right.value) {
-		const code = trim(`
-			# ${root.operator}
-			movl $${root.left.value}, %eax
-			${asm_instruction[root.operator]} $${root.right.value}, %eax
-		`);
-		return code; 
-	} else if (!root.left.value && root.right.value) {
-		const left = test(root.left);
-		const right = root.right.value;
-		return trim(`
-			${left}
+        if (root.left.value && root.right.value) {
+                const code = trim(`
+                        # ${root.operator}
+                        movl $${root.left.value}, %eax
+                        ${asm_instruction[root.operator]} $${root.right.value}, %eax
+                `);
+                return code; 
+        } else if (!root.left.value && root.right.value) {
+                const left = test(root.left);
+                const right = root.right.value;
+                return trim(`
+                        ${left}
 
-			# ${root.operator}
-			${asm_instruction[root.operator]} $${right}, %eax
-		`);
-	} else if (root.left.value && !root.right.value) {
-		const left = root.left.value;
-		const right = test(root.right);
-		return trim(`
-			${right}
+                        # ${root.operator}
+                        ${asm_instruction[root.operator]} $${right}, %eax
+                `);
+        } else if (root.left.value && !root.right.value) {
+                const left = root.left.value;
+                const right = test(root.right);
+                return trim(`
+                        ${right}
 
-			# ${root.operator}
-			${asm_instruction[root.operator]} $${left}, %eax
-		`);
+                        # ${root.operator}
+                        ${asm_instruction[root.operator]} $${left}, %eax
+                `);
 
-	} else {
-		++stack_size;
-		const left = test(root.left);
-		const right = test(root.right);
+        } else {
+                const left = test(root.left);
+                const right = test(root.right);
+                return trim(`
+                        ${left}
+                        movl %eax, -${(stack_size)*4}(%ebp)
 
-		return trim(`
-			${left}
-			movl %eax, -${(stack_size)*4}(%ebp)
-			
-			${right}
-			${asm_instruction[root.operator]} -${stack_size*4}(%ebp), %eax
-		`);
-	}
+                        ${right}
+                        ${asm_instruction[root.operator]} -${(stack_size)*4}(%ebp), %eax
+                `);
+        }
 }
 
 const asm_code = test(ast);
 console.log(
-	trim(
-		`
-		subl $${stack_size*4}, %esp
-		
-		${asm_code}
+        trim(
+                `
+                subl $${stack_size*4}, %esp
 
-		addl $${stack_size*4}, %esp
-		`
-	)
+                ${asm_code}
+
+                addl $${stack_size*4}, %esp
+                `
+        )
 );
